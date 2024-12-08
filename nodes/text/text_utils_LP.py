277b2878725d442385be501c17b18def871d5a89
Level Pixel Dev <levelpixel.dev@gmail.com>
2024-12-09 05:04:06 +0600

    Added autotranslator
    Added autotranslator based on Google Translate

--- nodes/text/text_utils_LP.py
+++ nodes/text/text_utils_LP.py
@@ -1,6 +1,8 @@
 import re
 import random
 import time
+from deep_translator import GoogleTranslator
+from langdetect import detect
 
 class TextChoiceParser:
     @classmethod
@@ -51,11 +53,55 @@ class TextChoiceParser:
     def IS_CHANGED(s, text, variables="", seed=None):
         return (text, variables, seed)
 
+class CLIPTextEncodeTranslate:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"text": ("STRING", {"multiline": True, "dynamicPrompts": True}), "clip": ("CLIP", )}}
+    RETURN_TYPES = ("CONDITIONING",)
+    FUNCTION = "clip_text_encode_translate"
+    CATEGORY = "LevelPixel/Text"
+
+    def clip_text_encode_translate(self, clip, text):
+        if text.strip():
+            detected_lang = detect(text)
+            if detected_lang != 'en':
+                try:
+                    translator = GoogleTranslator(source='auto', target='en')
+                    text = translator.translate(text)
+                except Exception as e:
+                    print(f"Translation error: {e}")
+        tokens = clip.tokenize(text)
+        cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
+        return ([[cond, {"pooled_output": pooled}]], )
+
+class TextTranslate:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"text": ("STRING", {"default": "text", "multiline": True})}}
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("text",)
+    FUNCTION = "text_translate"
+    CATEGORY = "LevelPixel/Text"
+
+    def text_translate(self, prompt):
+        if prompt.strip():
+            detected_lang = detect(prompt)
+            if detected_lang != 'en':
+                try:
+                    translator = GoogleTranslator(source='auto', target='en')
+                    prompt = translator.translate(prompt)
+                except Exception as e:
+                    print(f"Translation error: {e}")
+        return (prompt,)
+
 NODE_CLASS_MAPPINGS = {
     "TextChoiceParser|LP": TextChoiceParser,
+    "CLIPTextEncodeTranslate|LP": CLIPTextEncodeTranslate,
+    "TextTranslate|LP": TextTranslate,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "TextChoiceParser|LP": "Text Choice Parser [LP]",
+    "CLIPTextEncodeTranslate|LP": "CLIP Text Encode Translate [LP]",
+    "TextTranslate|LP": "Text Translate [LP]",
 }
-
